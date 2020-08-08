0473bee243a251f4b2216de3c41ac1e6c97206d4
Eric Wolz <ericwol@microsoft.com>
2020-08-07 17:51:04 -0700

    Initial checkin

---
+++ firmware/src/iot_config/cloud_config.h
@@ -0,0 +1,14 @@
+#ifndef CLOUD_CONFIG_H
+#define CLOUD_CONFIG_H
+
+// <h> Cloud Configuration
+
+// <s> project id
+// <i> Azure IoT Hub
+// <id> project_id
+#define CFG_PROJECT_ID "pic-iot"
+
+
+// </h>
+
+#endif // CLOUD_CONFIG_H
