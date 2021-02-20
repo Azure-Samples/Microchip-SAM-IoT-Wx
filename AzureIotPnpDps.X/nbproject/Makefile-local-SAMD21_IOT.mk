541d6461506a4030d6621b8037e597f30b45e8e7
Daisuke Nakahara <daisuken@msn.com>
2021-02-20 04:38:03 -0800

    Restructure project

--- AzureIotPnpDps.X/nbproject/Makefile-local-SAMD21_IOT.mk
+++ AzureIotPnpDps.X/nbproject/Makefile-local-SAMD21_IOT.mk
@@ -21,19 +21,19 @@ PATH:=C:/Program Files (x86)/Microchip/MPLABX/v5.40/mplab_platform/platform/../m
 # Path to java used to run MPLAB X when this makefile was created
 MP_JAVA_PATH="C:\Program Files (x86)\Microchip\MPLABX\v5.40\sys\java\zulu8.36.0.1-ca-fx-jdk8.0.202-win_x64\jre/bin/"
 OS_CURRENT="$(shell uname -s)"
-MP_CC="C:\Program Files\Microchip\xc32\v2.41\bin\xc32-gcc.exe"
-MP_CPPC="C:\Program Files\Microchip\xc32\v2.41\bin\xc32-g++.exe"
+MP_CC="C:\Program Files\Microchip\xc32\v2.50\bin\xc32-gcc.exe"
+MP_CPPC="C:\Program Files\Microchip\xc32\v2.50\bin\xc32-g++.exe"
 # MP_BC is not defined
-MP_AS="C:\Program Files\Microchip\xc32\v2.41\bin\xc32-as.exe"
-MP_LD="C:\Program Files\Microchip\xc32\v2.41\bin\xc32-ld.exe"
-MP_AR="C:\Program Files\Microchip\xc32\v2.41\bin\xc32-ar.exe"
+MP_AS="C:\Program Files\Microchip\xc32\v2.50\bin\xc32-as.exe"
+MP_LD="C:\Program Files\Microchip\xc32\v2.50\bin\xc32-ld.exe"
+MP_AR="C:\Program Files\Microchip\xc32\v2.50\bin\xc32-ar.exe"
 DEP_GEN=${MP_JAVA_PATH}java -jar "C:/Program Files (x86)/Microchip/MPLABX/v5.40/mplab_platform/platform/../mplab_ide/modules/../../bin/extractobjectdependencies.jar"
-MP_CC_DIR="C:\Program Files\Microchip\xc32\v2.41\bin"
-MP_CPPC_DIR="C:\Program Files\Microchip\xc32\v2.41\bin"
+MP_CC_DIR="C:\Program Files\Microchip\xc32\v2.50\bin"
+MP_CPPC_DIR="C:\Program Files\Microchip\xc32\v2.50\bin"
 # MP_BC_DIR is not defined
-MP_AS_DIR="C:\Program Files\Microchip\xc32\v2.41\bin"
-MP_LD_DIR="C:\Program Files\Microchip\xc32\v2.41\bin"
-MP_AR_DIR="C:\Program Files\Microchip\xc32\v2.41\bin"
+MP_AS_DIR="C:\Program Files\Microchip\xc32\v2.50\bin"
+MP_LD_DIR="C:\Program Files\Microchip\xc32\v2.50\bin"
+MP_AR_DIR="C:\Program Files\Microchip\xc32\v2.50\bin"
 # MP_BC_DIR is not defined
+DFP_DIR=C:/Program Files (x86)/Microchip/MPLABX/v5.40/packs/Microchip/SAMD21_DFP/3.3.98
 CMSIS_DIR=C:/Program Files (x86)/Microchip/MPLABX/v5.40/packs/arm/CMSIS/5.4.0
-DFP_DIR=C:/Users/jasminel/.mchp_packs/Microchip/SAMD21_DFP/3.3.98
